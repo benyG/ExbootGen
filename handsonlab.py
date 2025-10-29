6944ae09007150bfe81f64e63936f2d598058a8b
TheBenyGreen <eyesopencrew@gmx.com>
2025-10-29 13:55:08 -0400

    Add files via upload

---
+++ handsonlab.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def player():
+    return render_template("player.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
